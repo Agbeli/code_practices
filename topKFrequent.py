18ac4cadb3e13c9be94332cad93e50d11ae55382
Agbeli <agbeliemmanuel@gmail.com>
2023-01-22 23:16:08 +0000

    topk2

--- topKFrequent.py
+++ topKFrequent.py
@@ -31,4 +31,10 @@ if __name__ == '__main__':
 
 	print(f"top {k} of {nums} is {topK.topKfrequent(nums,k)}")
 
+	#### testcase 2:
+
+	nums2,k2 = [1], 1
+
+	print(f"top {k2} of {nums2} is {topK.topKfrequent(nums2,k2)}")
+
 
