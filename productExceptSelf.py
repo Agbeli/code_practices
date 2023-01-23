9f0142c4868a66241df8a068bab614b76c87c69f
Agbeli <agbeliemmanuel@gmail.com>
2023-01-23 18:02:39 +0000

    productExceptSelf

---
+++ productExceptSelf.py
@@ -0,0 +1,43 @@
+from typing import List 
+
+
+
+
+
+
+class ProductExceptSelf:
+	def _productExceptSelf(self,nums:List[int])->List[int]:
+
+		product = [1] * len(nums) #### initialize the indices with 1 
+
+		preproduct = 1  ### prefix the product with 1 
+
+		postproduct = 1  ### postfix the product with 1 
+
+		
+		for i in range(len(nums)):
+
+			product[i] = preproduct
+
+			preproduct *= nums[i]
+
+		for i in range(len(nums)-1,-1,-1):
+
+			product[i] *= postproduct
+
+			postproduct *= nums[i]
+
+		return product
+
+
+
+
+if __name__ == '__main__':
+
+	prodExceptSelf = ProductExceptSelf()
+
+	##### test case 
+
+	nums = [1,2,3,4]
+
+	print(f"product Except Self of {nums} is {prodExceptSelf._productExceptSelf(nums)}")
