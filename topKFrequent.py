a478a59cefbc7d0e01b1c9f96c6179ecba1fc6f0
Agbeli <agbeliemmanuel@gmail.com>
2023-01-31 16:31:18 +0000

    topKFrequent.py

--- topKFrequent.py
+++ topKFrequent.py
@@ -9,7 +9,7 @@ class TopKfrequent:
 	def topKfrequent(self,nums:List[int],k:int)->List[int]:
 
 		"""
-		nums : input a list of integers 
+        nums : input a list of integers 
 
 		k : topmost frequent integer in the list. 
 
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 	#### instantiate the class: ####### 
 	topK = TopKfrequent()
 
-	#### testcase: ####### 
+	#### testcase 1: ####### 
 
 	nums, k = [1,1,1,2,2,3], 2
 
@@ -50,5 +50,4 @@ if __name__ == '__main__':
 	nums2,k2 = [1], 1
 
 	print(f"top {k2} of {nums2} is {topK.topKfrequent(nums2,k2)}")
-
 
