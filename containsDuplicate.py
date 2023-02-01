9f433127e63e1d8a46ee63d1ffd6c303b8f3e14d
Agbeli <agbeliemmanuel@gmail.com>
2023-02-01 11:06:52 +0000

    containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -17,7 +17,10 @@ class ContainsDuplicate:
 		self.nums = nums 
 
 	def checkDuplicate(self)->bool:
-
+		
+		"""
+		return : boolean output 
+		"""
 		singleNum = set()  ### instantiate hash set to store unique numbers  
 
 		for num in self.nums:
@@ -30,15 +33,21 @@ class ContainsDuplicate:
 
 
 if __name__ == '__main__':
+	
+	##### testcase 1: 
 	nums1 = [1,2,3,1]
 	constainsElement = ContainsDuplicate(nums=nums1)
 	print(f"The {nums1} has duplicate elements: {constainsElement.checkDuplicate()}")
+
 	print()
+	
+	##### testcase 2:
 	nums2 = [1,2,3,4]
 	constainsElement2 = ContainsDuplicate(nums=nums2)
 	print(f"The {nums2} has duplicate elements: {constainsElement2.checkDuplicate()}")
 
 	print()
+	##### testcase 3: 
 	nums3 = [1,1,1,3,3,4,3,2,4,2]
 	constainsElement3 = ContainsDuplicate(nums=nums3)
 	print(f"The {nums3} has duplicate elements: {constainsElement3.checkDuplicate()}")
