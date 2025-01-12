6b236d9fb8a1d7c0e6341c22a2d6dc25cd532998
Agbeli <agbeliemmanuel@gmail.com>
2025-01-12 01:32:12 -0600

    update file

--- linked_list.py
+++ linked_list.py
@@ -10,6 +10,11 @@ class LinkedList:
 	def __init__(self):
 		self.head = None 
 	
+	def printList(self):
+		curr_node = self.head
+		while curr_node:
+			print("here is the data: ",curr_node.data)
+			curr_node = curr_node.next	
 	def insertNode(self,node):
 
 		if self.head is None:
@@ -18,6 +23,21 @@ class LinkedList:
 			lastNode = self.head
 			while lastNode.next:
 				lastNode = lastNode.next
-			lastNode = node
+			lastNode.next = node
 
-	
+	def prepend(self,node):
+		if self.head is None:
+			self.head = node
+			return
+		node.next = self.head
+		self.head = node 
+		
+if __name__=='__main__':
+	node1 = Node(10)
+	node2 = Node(20)
+	node3 = Node(100)
+	llistnode = LinkedList()
+	llistnode.insertNode(node1)
+	llistnode.insertNode(node2)
+	llistnode.prepend(node3)
+	llistnode.printList()
