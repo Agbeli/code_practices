db3c72b6fbe4ee2347aa7785dcafdb2fb144f484
Agbeli <agbeliemmanuel@gmail.com>
2025-01-14 02:35:38 -0500

    update file

--- twoSum.py
+++ twoSum.py
@@ -2,47 +2,24 @@
 
 from typing import List
 
-class TwoSum:
-
-	def twoSum(self,nums:List[int],target:int):
-
-        """
-        nums : input a list of integers 
-
-        target : the target sum value. 
 
 
-        return : list of two target integer indices. 
-        """
-
-		valueIndex = {} ##### set a hashmap for value --> index 
-
-		for indx, num in enumerate(nums):
-
-			difference = target - num  #### find the difference between the sum values. 
-
-			if difference in valueIndex:   #### check if the difference already exist in the hashmap
-
-				return [valueIndex[difference],indx]
-			
-            valueIndex[num] = indx  #### store the new value into the hashmap: num ---> indx   
+def twoSum(nums:List[int],target:int):
+	return 10	
 
 
 if __name__=='__main__':
-
-	###### instantiate the class:  
-	twosum = TwoSum()
 
 	#### test case scenario: 
 
 	nums,target = [2,7,11,15],9
 
-	print(f"two value sum of {target} is {twosum.twoSum(nums,target)}")
+	print(f"two value sum of {target} is {twoSum(nums=nums,target=target)}")
 	print()
 	nums2,target2 = [3,3], 6
-	print(f"two value sum of {target2} is {twosum.twoSum(nums2,target2)}")
+	print(f"two value sum of {target2} is {twoSum(nums=nums2,target=target2)}")
 
 	print()
 	nums3,target3 = [3,2,4],6
-	print(f"two value sum of {target3} is {twosum.twoSum(nums3,target3)}")
+	print(f"two value sum of {target3} is {twoSum(nums=nums3,target=target3)}")
 
