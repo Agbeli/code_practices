cd51a39ba8f5afbaefabc6f556dd06493eaf60e5
Emmanuel Agbeli <emmanuelagbeli@Mac.lan>
2025-06-10 19:54:07 -0400

    updated moveZeros code

--- moveZeros.py
+++ moveZeros.py
@@ -1,4 +1,3 @@
-
 from typing import List	
 
 
@@ -8,12 +7,25 @@ def moveZeros(nums:List):
 	size = len(nums)
 	
 
+	for indx in range(0,size):
+		if nums[indx] != 0:
+			
+			temp = nums[lastZeroNum]
+			nums[lastZeroNum] = nums[indx]
+			lastZeroNum += 1
+			nums[indx] = temp 
+	return nums	
+
 
 
 
 if __name__ == "__main__":
 	nums = [0,1,3,0,5]
 	print(moveZeros(nums))
+	print("*" * 20)
+	nums2 = [4,0,8,0,0,20]
+	print(moveZeros(nums2))
+
 
 
 
