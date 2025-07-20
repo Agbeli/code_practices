7ffe5ba751a2824031dfdc0c38fbb32ee09fd99e
Agbeli <agbeliemmanuel@gmail.com>
2025-07-19 23:34:46 -0400

    update file move zeros

--- moveZeros.py
+++ moveZeros.py
@@ -28,14 +28,14 @@ if __name__ == "__main__":
 	## sample cases  
 	nums = [0,1,3,0,5]
 	print(moveZeros(nums))
-	print("*" * 20)
+	print("#" * 20)
 	nums2 = [4,0,8,0,0,20]
 	print(moveZeros(nums2))
-	print("*" * 20)
+	print("#" * 20)
 	## weird case example 
 	nums3 = [3,4,2,9,0,0,0]
 	print(moveZeros(nums3))
-	print("*"* 20) 
+	print("#"* 20) 
 
 
 
